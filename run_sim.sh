85299dface922e74cd53ad3cf532ba505aa3bd9e
Duy (Finn) Nguyen <nguyenduduy233@gmail.com>
2026-07-20 16:52:21 +1200

    chore: remove absolute path

--- run_sim.sh
+++ run_sim.sh
@@ -1,4 +1,4 @@
 ORIGINAL_DIR=$(pwd)
-cd /home/duy/RPL-TinyML/contiki-ng/tools/cooja/
-./gradlew run --args='--no-gui /home/duy/RPL-TinyML/RPL.csc --logdir=/home/duy/RPL-TinyML/simulation_logs'
+cd ./contiki-ng/tools/cooja/
+./gradlew run --args='--no-gui ../../../RPL.csc --logdir=../../../simulation_logs'
 cd $ORIGINAL_DIR
