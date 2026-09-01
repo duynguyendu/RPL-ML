d34dc1e24a4c7fe7b8346710a656be283cb415fa
Duy (Finn) Nguyen <nguyenduduy233@gmail.com>
2026-09-02 00:20:39 +1200

    sim: update param

--- scripts/simulate.sh
+++ scripts/simulate.sh
@@ -1,7 +1,7 @@
 # Run pipeline.py for a range of node counts
 
-for NUM_NODES in 60 80; do
-    for SEND_RATE in 1 3 5 10 20 30 60; do
+for NUM_NODES in 40 60 80; do
+    for SEND_RATE in 5 10 15 20; do
         for RPL_OF in of0 mhrof; do
             echo "=== Running pipeline.py with num_of_nodes=$NUM_NODES, send_rate=$SEND_RATE ==="
             python3 pipeline.py \
